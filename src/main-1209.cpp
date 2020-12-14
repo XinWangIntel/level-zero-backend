4e117ad98577ef3bafde1b2f027ef9ffa9f92789
xin1.wang <xin1.wang@intel.com>
2020-12-14 11:15:17 -0500

    Add more event control and test context can be copyed
    Signed-off-by: xin1.wang <xin1.wang@intel.com>

--- src/main-1209.cpp
+++ src/main-1209.cpp
@@ -24,36 +24,14 @@ int main(int argc, char **argv) {
    ze_memory_type_t memory_type = ZE_MEMORY_TYPE_HOST;
   //ze_memory_type_t memory_type = ZE_MEMORY_TYPE_SHARED;
   int offset = 0;
-
-  std::vector<ze_device_handle_t> supportedDevices;
   for (auto driver : lzt::get_all_driver_handles()) {
     for (auto device : lzt::get_devices(driver)) {
-      supportedDevices.push_back(device);
-    }
-  }
-  std::cout << "xin supportedDevice.size:" << supportedDevices.size() << std::endl;
-  ze_context_handle_t context1 = lzt::get_default_context();
-  ze_device_handle_t device1 = supportedDevices[0];
-  ze_context_handle_t context = context1;
-  ze_device_handle_t device = device1;
-  ze_command_queue_handle_t command_queue = lzt::create_command_queue(context, device, 0,
-                                    ZE_COMMAND_QUEUE_MODE_DEFAULT,
-                                    ZE_COMMAND_QUEUE_PRIORITY_NORMAL, 0);
-  ze_command_list_handle_t command_list = lzt::create_command_list(context, device, 0, 0);
-  std::vector<uint8_t> binary_file = lzt::load_binary_file("spirv_0");
-  ze_module_handle_t module = lzt::create_module(context,
-                                                 device,
-                                                 binary_file.data(),
-                                                 binary_file.size(),
-                                                 ZE_MODULE_FORMAT_IL_SPIRV,
-                                                 "",
-                                                 nullptr);
-  
-  //auto module = lzt::create_module(device, "spirv_0");
-  ze_kernel_handle_t kernel = lzt::create_function(module, "main_kernel");
-  
-  {
-    {
+      // set up
+      auto command_queue = lzt::create_command_queue();
+      auto command_list = lzt::create_command_list();
+
+      auto module = lzt::create_module(device, "spirv_0");
+      auto kernel = lzt::create_function(module, "main_kernel");
 
       int64_t *input_data, *input_data1, *output_data;
       if (memory_type == ZE_MEMORY_TYPE_HOST) {
@@ -71,22 +49,18 @@ int main(int argc, char **argv) {
         output_data = static_cast<int64_t *>(
             lzt::allocate_shared_memory(size * sizeof(int64_t)));
       }
-      std::cout << "xin:" << __LINE__ << std::endl;
 
-      // for(int i = 0; i < size; i++) {
-      //    input_data[i] = i;
-      //    input_data1[i] = i;
-     // }
-      std::cout << "xin:" << __LINE__ << std::endl;
+       for(int i = 0; i < size; i++) {
+          input_data[i] = i;
+          input_data1[i] = i;
+      }
       lzt::zeEventPool eventPool;
-      eventPool.InitEventPool(context, 32);
       std::vector<uint64_t> value0 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
       ze_event_handle_t e0;
       eventPool.create_event(e0);
       lzt::append_memory_copy(command_list, (void *)input_data,
                               (void *)value0.data(), 9 * 8, e0, 0, nullptr);
 
-      std::cout << "xin:" << __LINE__ << std::endl;
       std::vector<uint64_t> value1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
       ze_event_handle_t e1;
       eventPool.create_event(e1);
@@ -99,14 +73,10 @@ int main(int argc, char **argv) {
       lzt::append_memory_copy(command_list, (void *)output_data,
                               (void *)out.data(), 9 * 8, e2, 0, nullptr);
 
-      std::cout << "xin:" << __LINE__ << std::endl;
-      std::cout << "xin: input_data size:" << sizeof(input_data) << std::endl;
-      ze_kernel_handle_t kernel1 = kernel;
-      lzt::set_argument_value(kernel1, 0, sizeof(input_data), &input_data);
-      lzt::set_argument_value(kernel1, 1, sizeof(input_data1), &input_data1);
-      lzt::set_argument_value(kernel1, 2, sizeof(output_data), &output_data);
+      lzt::set_argument_value(kernel, 0, sizeof(input_data), &input_data);
+      lzt::set_argument_value(kernel, 1, sizeof(input_data1), &input_data1);
+      lzt::set_argument_value(kernel, 2, sizeof(output_data), &output_data);
 
-      std::cout << "xin:" << __LINE__ << std::endl;
       lzt::set_group_size(kernel, 1, 9, 9);
 
       ze_group_count_t group_count;
@@ -123,7 +93,6 @@ int main(int argc, char **argv) {
       lzt::append_launch_function(command_list, kernel, &group_count, e3,
                                   events.size(), events.data());
 
-      std::cout << "xin:" << __LINE__ << std::endl;
       //lzt::close_command_list(command_list);
       //lzt::execute_command_lists(command_queue, 1, &command_list, nullptr);
       //lzt::synchronize(command_queue, UINT64_MAX);
@@ -156,12 +125,10 @@ int main(int argc, char **argv) {
                               (void *)output_data, 9 * 8, e2_2, events2.size(),
                               events2.data());
 
-      std::cout << "xin:" << __LINE__ << std::endl;
       lzt::close_command_list(command_list);
       lzt::execute_command_lists(command_queue, 1, &command_list, nullptr);
       lzt::synchronize(command_queue, UINT64_MAX);
 
-      std::cout << "xin:" << __LINE__ << std::endl;
       // if (0 != memcmp(input_data, output_data + offset, (size - offset) *
       // sizeof(int)))
       //    return -1;
@@ -172,9 +139,9 @@ int main(int argc, char **argv) {
       zeEventHostSynchronize(et0, UINT64_MAX);
       zeEventHostSynchronize(et1, UINT64_MAX);
       zeEventHostSynchronize(et2, UINT64_MAX);
-      zeEventHostSynchronize(e0_0, UINT64_MAX);
-      zeEventHostSynchronize(e1_1, UINT64_MAX);
-      zeEventHostSynchronize(e2_2, UINT64_MAX);
+      //zeEventHostSynchronize(e0_0, UINT64_MAX);
+      //zeEventHostSynchronize(e1_1, UINT64_MAX);
+      //zeEventHostSynchronize(e2_2, UINT64_MAX);
 
       for (int i = 0; i < size; i++) {
         std::cout << output_data[i] << " ";
@@ -184,7 +151,6 @@ int main(int argc, char **argv) {
         std::cout << out[i] << " ";
       }
 
-      lzt::synchronize(command_queue, UINT64_MAX);
       // cleanup
       lzt::free_memory(input_data);
       lzt::free_memory(output_data);
